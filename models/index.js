da25381b01f076ff749871c043c9f6cb08736bdb
BSord <brandon.sorgdrager@gmail.com>
2020-08-29 21:48:13 +0000

    Add better mongo error handling

--- models/index.js
+++ models/index.js
@@ -4,7 +4,15 @@ const mongoose = require('mongoose');
 const Tile = require('./tile');
 
 const connectDb = () => {
-  return mongoose.connect(process.env.MongoURI, { useNewUrlParser: true });
+  mongoose.connect(process.env.MongoURI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }).
+  catch(error => console.log(error)); // Use new url parser to supress warnings
+
+  mongoose.set('useCreateIndex', true); // Hide warnings about deprecation of 'ensureIndex'
+  mongoose.Promise = global.Promise;
+  mongoose.connection.on('error', error => {
+    console.log(error);
+  });
+  return mongoose;
 };
 
 const models = { Tile };
