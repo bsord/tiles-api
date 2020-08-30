21b183465f9414ab545df756dbe0cf61eea93e7d
BSord <brandon.sorgdrager@gmail.com>
2020-08-30 23:51:59 +0000

    fix mongo

--- models/index.js
+++ models/index.js
@@ -4,15 +4,7 @@ const mongoose = require('mongoose');
 const Tile = require('./tile');
 
 const connectDb = () => {
-  mongoose.connect(process.env.MongoURI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }).
-  catch(error => console.log(error)); // Use new url parser to supress warnings
-
-  mongoose.set('useCreateIndex', true); // Hide warnings about deprecation of 'ensureIndex'
-  mongoose.Promise = global.Promise;
-  mongoose.connection.on('error', error => {
-    console.log(error);
-  });
-  return mongoose;
+  return mongoose.connect(process.env.MongoURI, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
 };
 
 const models = { Tile };
