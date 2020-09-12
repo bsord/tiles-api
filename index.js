06575a20fae6f3e5040df67e9bb4cdcad3cd28e6
mgmtadmin <brandon.sorgdrager@gmail.com>
2020-09-12 16:58:50 -0500

    Update redis connection info

--- index.js
+++ index.js
@@ -23,8 +23,17 @@ const io = socketIo(server);
 
 // Tell socket.io to use redis adapter if specified
 if(process.env.Redis_Hostname){
-  var redis = require('socket.io-redis');
-  io.adapter(redis({ host: process.env.Redis_Hostname, port: 6379 }));
+  if(process.env.Redis_Password){
+    const redis = require('redis');
+    const redisAdapter = require('socket.io-redis');
+    const pub = redis.createClient(6379, process.env.Redis_Hostname, { auth_pass: process.env.Redis_Password });
+    const sub = redis.createClient(6379, process.env.Redis_Hostname, { auth_pass: process.env.Redis_Password });
+    io.adapter(redisAdapter({ pubClient: pub, subClient: sub }));
+  } else {
+    var redis = require('socket.io-redis');
+    io.adapter(redis({ host: process.env.Redis_Hostname, port: 6379 }));
+  }
+  
 }
 
 // Tell socket events to go to socket handler. Similar to how routes work
